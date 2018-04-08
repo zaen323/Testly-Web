7af795543e884d040fa5ca6daa3edf0150f90728
aidatorajiro <kawarusosu@zoho.com>
2018-04-08 15:35:04 +0900

    installed nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Testly',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Testly web version' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' }
+}
